7fd7760c79f8dd3a6cc55376126b995a9a694e87
ninjafox <antherynx.studio@mail.com>
2018-01-19 01:22:06 -0500

    ah yeah and this.

--- build/jsmn.d
+++
@@ -1,5 +0,0 @@
-jsmn.o: \
- c:/Users/HolgerBirgitLukasPau/Desktop/RS/foxverse-installer/source/jsmn.c \
- c:/Users/HolgerBirgitLukasPau/Desktop/RS/foxverse-installer/source/jsmn.h
-
-c:/Users/HolgerBirgitLukasPau/Desktop/RS/foxverse-installer/source/jsmn.h:
